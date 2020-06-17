3c633681e0570b54ad7daebc1c38c90c2fb280d5
RonaldBrand <19157169@student.hhs.nl>
2020-06-17 12:52:31 +0200

    created Galaxy class

--- GalaxyQuest/src/io/github/jornegitgud/galaxyquest/Galaxy.java
+++ GalaxyQuest/src/io/github/jornegitgud/galaxyquest/Galaxy.java
@@ -1,4 +1,14 @@
 package io.github.jornegitgud.galaxyquest;
 
 public class Galaxy {
+
+    private GalaxySettings settings;
+    private String name;
+
+
+    public Galaxy(String name, GalaxySettings settings) {
+        this.name = name;
+        this.settings = settings;
+    }
+
 }
