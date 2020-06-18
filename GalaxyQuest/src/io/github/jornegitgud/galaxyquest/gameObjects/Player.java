e24836e7750c5b5e2e6fc340315873f84b2fad0c
Jordy de Koning <jordy.de.koning@s5.nl>
2020-06-18 22:42:05 +0200

    Move code in classes modified by Jorne to moved files

--- GalaxyQuest/src/io/github/jornegitgud/galaxyquest/gameObjects/Player.java
+++ GalaxyQuest/src/io/github/jornegitgud/galaxyquest/gameObjects/Player.java
@@ -1,4 +1,17 @@
 package io.github.jornegitgud.galaxyquest.gameObjects;
 
-public class Player {
+import io.github.jornegitgud.galaxyquest.Direction;
+import io.github.jornegitgud.galaxyquest.DirectionalSpriteList;
+import io.github.jornegitgud.galaxyquest.Tile;
+
+public class Player extends MovableObject {
+    String name;
+    Direction direction;
+
+    public Player(String name, Tile tile, Direction direction, DirectionalSpriteList directionalSpriteList) {
+        super(directionalSpriteList, tile);
+
+        this.name = name;
+        this.direction = direction;
+    }
 }
--- GalaxyQuest/src/io/github/jornegitgud/galaxyquest/gameObjects/SpacePirate.java
+++ GalaxyQuest/src/io/github/jornegitgud/galaxyquest/gameObjects/SpacePirate.java
@@ -1,4 +1,14 @@
 package io.github.jornegitgud.galaxyquest.gameObjects;
 
-public class SpacePirate {
+import io.github.jornegitgud.galaxyquest.Direction;
+import io.github.jornegitgud.galaxyquest.DirectionalSpriteList;
+import io.github.jornegitgud.galaxyquest.Tile;
+
+public class SpacePirate  extends MovableObject{
+    Direction direction;
+
+    public SpacePirate(Direction direction, Tile tile, DirectionalSpriteList directionalSpriteList) {
+        super(directionalSpriteList, tile);
+        this.direction = direction;
+    }
 }
